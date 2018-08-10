2e470c19489dc51321d099be6d81c54b888df93f
Musa Musayev <musa@musayev.io>
2018-08-09 22:06:28 -0400

    Added terraform.tfvars and broke out variables

--- Ghost/variables.tf
+++ Ghost/variables.tf
@@ -7,33 +7,17 @@ variable "env" {
 variable "image" {
   description = "Image for Container"
   type = "map"
-  default = {
-    dev = "ghost:latest"
-    prod = "ghost:alpine"
-  }
 }
 
 variable "container_name" {
   description = "Name of Docker Container"
   type = "map"
-  default {
-    dev = "dev_blog"
-    prod = "prod_blog"
-  }
 }
 
 variable "int_port" {
   type = "map"
-  default = {
-    dev = "2368"
-    prod = "2368"
-  }
 }
 
 variable "ext_port" {
   type = "map"
-  default = {
-    dev = "8080"
-    prod = "80"
-  }
 }
